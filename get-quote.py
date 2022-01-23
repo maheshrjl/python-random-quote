5bac9e27c33b9d4a96b185b01bdb25d9b662eb9b
unknown <62394512+maheshrjl@users.noreply.github.com>
2022-01-23 19:53:15 +0530

    quotes can be added programatically

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,6 @@
+from imaplib import Int2AP
 import random
+from re import X
 
 
 def primary():
@@ -11,8 +13,24 @@ def primary():
   last = 13
   rnd = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd],end="")
+  print(quotes[random.randint(0, last)])
 
   #print(quotes)
+
+def write():
+  print("Now we'll add your favourtie quote to the list:\n")
+  print("Enter a quote:")
+  f = open("quotes.txt", "a")
+  x = input()
+  print(f"Inserting to file now!")
+  quotes = f.write(x + "\n")
+
+  f.close()
+  print("\nInserted successfully!")
+
+  
 if __name__== "__main__":
+  print("\n First we'll display a random quote from the file. Then, you can add your own quote to the file.")
   primary()
+  write()
